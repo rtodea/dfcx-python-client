b9c76f7fcbc62caefe5cf90c4f13a9ff98b4415f
Robert Todea <robert.todea@gmail.com>
2024-05-19 03:36:14 +0000

    WIP

--- shell.nix
+++ shell.nix
@@ -1,9 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
+# shell.nix
+{ pkgs ? import <nixpkgs> {
+    config.allowUnfree = true;  # This allows unfree software
+  } 
+}:
 
 pkgs.mkShell {
-  buildInputs = [ pkgs.python312Full pkgs.stdenv.cc.cc ];
+  buildInputs = [ 
+    pkgs.python312Full 
+    pkgs.jetbrains.pycharm-professional 
+    pkgs.stdenv.cc.cc 
+  ];
   shellHook = ''
     export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
   '';
 }
-
